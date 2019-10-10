fdffe08c934c59d781923217ad8fa4255fb02360
Lemmon <47559770+ruka-l@users.noreply.github.com>
2019-10-10 20:46:20 +0900

    Update search.php

--- search.php
+++ search.php
@@ -3,13 +3,14 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 include ("simple_html_dom.php");
 $sedata = $_GET['search'];
+$sedata = urlencode($sedata);
 if ($sedata == "") {
 echo "검색어를 입력하세요";
 } else {
-$html = file_get_html('https://search.dcinside.com/combine/q/'.$sedata);
-/*foreach($html->find('p[class=link_dsc_txt]') as $e) {
-  echo $html->innertext;
-}*/
-echo $html;
+$html = file_get_html('https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query='.$sedata);
+foreach($html->find('a[class=api_txt_lines total_tit]') as $e) {
+  $bb = $html->innertext;
+  echo strip_tags($bb);
+}
 }
 ?>
