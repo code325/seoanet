e111a306135c840984e105f09171c5814315bc49
Lemmon <47559770+ruka-l@users.noreply.github.com>
2019-10-10 20:27:41 +0900

    Update search.php

--- search.php
+++ search.php
@@ -5,7 +5,7 @@ if ($sedata == "") {
 echo "검색어를 입력하세요";
 } else {
 $html = file_get_html('https://search.dcinside.com/combine/q/'.$sedata);
-foreach($html->find('div[class=link_dsc_txt]') as $e) {
+foreach($html->find('[class=link_dsc_txt]') as $e) {
   echo $html->innertext;
 }
 }
