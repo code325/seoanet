14c6b8e44094ddc4622756477d4f9e55757c7990
Lemmon <47559770+ruka-l@users.noreply.github.com>
2019-10-10 20:18:48 +0900

    Update search.php

--- search.php
+++ search.php
@@ -5,8 +5,6 @@ if ($sedata == "") {
 echo "검색어를 입력하세요";
 } else {
 $html = file_get_html('https://www.google.co.uk/search?hl=en&q='.$sedata);
-foreach($html->find('div[class=zlBHuf MUxGbd v0nnCb]') as $e) {
-    echo $e->innertext;
-}
+echo $html;
 }
 ?>
