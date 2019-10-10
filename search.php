6cc09b02d3ce98e43419ec8d679cf8d4de4c35cf
Lemmon <47559770+ruka-l@users.noreply.github.com>
2019-10-10 20:14:03 +0900

    Update search.php

--- search.php
+++ search.php
@@ -4,5 +4,9 @@ $sedata = $_GET['search'];
 if ($sedata == "") {
 echo "검색어를 입력하세요";
 } else {
-echo "검색어가 입력되었습니다!";
+$html = file_get_html('https://www.google.co.uk/search?hl=en&q='.$sedata);
+foreach($html->find('div[class=zlBHuf MUxGbd v0nnCb]') as $e) {
+    echo $e;
 }
+}
+?>
