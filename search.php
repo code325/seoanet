6687037cc951c0f44626ea7138eee6d72c941da4
Lemmon <47559770+ruka-l@users.noreply.github.com>
2019-10-10 20:58:43 +0900

    Update search.php

--- search.php
+++ search.php
@@ -1,8 +1,8 @@
 <?php
-ini_set("user_agent","Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0");
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 include ("simple_html_dom.php");
+ini_set("user_agent","Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0");
 $sedata = $_GET['search'];
 if ($sedata == "") {
 echo "검색어를 입력하세요";
