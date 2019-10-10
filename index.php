6522a11030c3838e6a7574dc6532778fda4ebd99
Lemmon <47559770+ruka-l@users.noreply.github.com>
2019-10-10 19:59:35 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <script>
+            function view() {
+                var input = document.getElementById("input").value;
+                document.write(input);
+            }
+        </script>
+        <div class="nav">
+            Seoa Planet
+        </div>
+        <div class="content">
+            <div class="wrap">
+            <span id="logo">Seoa Planet</span>
+               <div class="search">
+                    <input type="text" class="searchTerm" placeholder="검색어를 입력하세요" id="input">
+                    <button type="submit" class="searchButton" onclick="view();">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
