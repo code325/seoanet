d3cd99841d45ebe2ce5d5173d7239fdcff8a3a54
Lemmon <47559770+ruka-l@users.noreply.github.com>
2019-10-10 20:32:29 +0900

    Update search.php

--- search.php
+++ search.php
@@ -6,7 +6,12 @@ $sedata = $_GET['search'];
 if ($sedata == "") {
 echo "검색어를 입력하세요";
 } else {
-$html = file_get_html('https://search.dcinside.com/combine/q/'.$sedata);
+$context = stream_context_create(array(
+    'http' => array(
+        'header' => array('User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201'),
+    ),
+));
+$html = file_get_html('https://search.dcinside.com/combine/q/'.$sedata, false, $context);
 foreach($html->find('p[class=link_dsc_txt]') as $e) {
   echo $html->innertext;
 }
